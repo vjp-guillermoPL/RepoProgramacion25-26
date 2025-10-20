dbcc59f97ced5dd95a8872d9eb42c04e18621d66
vjp-guillermoPL <gproll01@informatica.iesvalledeljerteplasencia.es>
2025-10-20 16:29:38 +0200

    Ejercicio27

--- ejercicio27MenuExcepciones/src/ejercicio27menuexcepciones/Ejercicio27MenuExcepciones.java
+++ ejercicio27MenuExcepciones/src/ejercicio27menuexcepciones/Ejercicio27MenuExcepciones.java
@@ -25,14 +25,34 @@ public class Ejercicio27MenuExcepciones {
         
         System.out.println("Introduce otro número");
         num2 = entrada.nextInt();
-        
+
+        do {
+            
         System.out.println("Elige la acción 1.- Sumar los números. 2.- Restar los números. 3.- Multiplicar los números. 4.- Dividir los números. 5.- Salir del programa.");
         control = entrada.nextInt();
         
         if(num2 == 0 && control == 4){
-        
+            try {
+                switch (control){
+            case 1: resultado = num1 + num2;
+                System.out.println(num1 +" + "+ num2 +" = "+ resultado);
+                break;
+            case 2: resultado = num1 - num2;
+                System.out.println(num1 +" - "+ num2 +" = "+ resultado);
+                break;
+            case 3: resultado = num1 * num2;
+                System.out.println(num1 +" * "+ num2 +" = "+ resultado);
+                break;
+            case 4: resultado = num1 / num2;
+                System.out.println(num1 +" / "+ num2 +" = "+ resultado);
+                break;
+            default: System.out.println("Introduzca un número el 1 al 5");
         }
-        do {
+            } catch(ArithmeticException e){
+                System.out.println("No puedes dividir por cero");
+                System.out.println(e);
+            }
+        }else 
         switch (control){
             case 1: resultado = num1 + num2;
                 System.out.println(num1 +" + "+ num2 +" = "+ resultado);
@@ -49,6 +69,7 @@ public class Ejercicio27MenuExcepciones {
             default: System.out.println("Introduzca un número el 1 al 5");
         }
         } while (control != 5);
+        
     }
     
 }
