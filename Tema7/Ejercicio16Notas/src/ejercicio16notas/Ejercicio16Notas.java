e7e39d20341915d716421a2480e160b06edaeb2c
vjp-guillermoPL <gproll01@informatica.iesvalledeljerteplasencia.es>
2026-01-20 16:46:01 +0100

    ejercicio 16 terminado

--- Tema7/Ejercicio16Notas/src/ejercicio16notas/Ejercicio16Notas.java
+++ Tema7/Ejercicio16Notas/src/ejercicio16notas/Ejercicio16Notas.java
@@ -19,7 +19,7 @@ public class Ejercicio16Notas {
         int clase[][] = new int[6][4];                                          //Creo el array
         String alumno[] = {"Pepe", "Juan", "Ana", "Marta", "Pedro", "María"};   //Creo el array con los nombres
         String asignatura[] = {"Lengua", "Mates", "Historia", "Física"};        //Creo el array con las asignaturas
-        
+
         int num;                                                                //Creo la variable
         Scanner entrada = new Scanner(System.in);
 
@@ -33,16 +33,16 @@ public class Ejercicio16Notas {
                     rellenarNotas(clase, alumno, asignatura);
                     break;
                 case 2:
-                    
+                    mostrarArray(clase, alumno, asignatura);
                     break;
                 case 3:
-                    
+                    mejorAlumno(clase, alumno);
                     break;
                 case 4:
-                    
+                    masSuspensos(clase, alumno);
                     break;
                 case 5:
-                    
+                    peorAsignatura(clase, asignatura);
                     break;
                 case 6:
                     System.out.println("Saliendo del programa...");
@@ -52,9 +52,9 @@ public class Ejercicio16Notas {
             }
             System.out.println("----------------------------------------------------------");
             System.out.println("Introduce un número entre el 1 y el 6");
-        } while (num != 5);
+        } while (num != 6);
     }
-    
+
     /**
      * Método que rellena el array con números que introduzca el usuario
      *
@@ -67,9 +67,106 @@ public class Ejercicio16Notas {
         for (int i = 0; i < clase.length; i++) {
             for (int j = 0; j < clase[i].length; j++) {
                 System.out.println("Introduzca una nota para el " + alumno[i] + " en la asignatura " + asignatura[j]);
-                clase[i][j] = entrada.nextInt();
+                //clase[i][j] = entrada.nextInt();
+                clase[i][j] = (int) (Math.random() * 10 + 1);
             }
         }
     }
+
+    /**
+     * Método que muestra todos los valores del array
+     *
+     * @param clase
+     * @param alumno
+     * @param asignatura
+     */
+    public static void mostrarArray(int clase[][], String alumno[], String asignatura[]) {
+        for (int i = 0; i < clase.length; i++) {
+            for (int j = 0; j < clase[i].length; j++) {
+                System.out.println("El alumno " + alumno[i] + " en la asignatura " + asignatura[j] + " tiene de nota un " + clase[i][j]);
+            }
+        }
+    }
+
+    /**
+     * Método que hace la media de cada alumno, guarda la mejor y la muestra por
+     * pantalla
+     *
+     * @param clase
+     * @param alumno
+     *
+     */
+    public static void mejorAlumno(int clase[][], String alumno[]) {
+        int suma = 0;
+        int mejorMedia = 0;
+        int mejorAlumno = 0;
+        for (int i = 0; i < clase.length; i++) {
+            for (int j = 0; j < clase[i].length; j++) {
+                suma += clase[i][j];
+                if (j == clase[0].length - 1) {
+                    suma = suma / clase[0].length;
+
+                    if (mejorMedia < suma) {
+                        mejorMedia = suma;
+                        mejorAlumno = i;
+                    }
+                    suma = 0;
+                }
+            }
+        }
+        System.out.println("El mejor alumno es " + alumno[mejorAlumno] + " con una media de " + mejorMedia);
+    }
+
+    /**
+     * Método que comprueba cual es el alumno con más suspensos
+     * @param clase
+     * @param alumno 
+     */
+    public static void masSuspensos(int clase[][], String alumno[]) {
+        int suspenso = 0;
+        int peorAlumno = 0;
+        int numSuspensos = 0;
+        for (int i = 0; i < clase.length; i++) {
+            for (int j = 0; j < clase[i].length; j++) {
+                if (clase[i][j] < 5) {
+                    suspenso ++;
+                }
+                
+                if (j == clase[0].length - 1){
+                    peorAlumno = i;
+                    numSuspensos = suspenso;
+                    
+                    suspenso = 0;
+                }
+            }
+        }
+        System.out.println("El peor alumno es "+ alumno[peorAlumno] +" con "+ numSuspensos +" suspensos");
+    }
     
+    /**
+     * Método que comprueba cual es la asignatura con la nota media más baja
+     * @param clase
+     * @param asignatura 
+     */
+    public static void peorAsignatura(int clase[][], String asignatura[]) {
+        int suma = 0;
+        int peorAsignatura = 0;
+        int peorMedia = 10;
+        for (int i = 0; i < clase.length; i++) {
+            for (int j = 0; j < clase[i].length; j++) {
+                suma += clase[i][j];
+                if (j == clase[0].length - 1) {
+                    suma = suma / clase[0].length;
+
+                    if (peorMedia > suma) {
+                        peorMedia = suma;
+                        peorAsignatura = i;
+                    }
+                    suma = 0;
+                }
+            }
+        }
+        System.out.println("La asignatura más dificil es "+ asignatura[peorAsignatura] +" con un "+ peorMedia +" de media");
+    }
+
 }
