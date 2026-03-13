0f04c2c317734bd8239db5390ff5e0a56b599231
vjp-guillermoPL <gproll01@informatica.iesvalledeljerteplasencia.es>
2026-03-13 19:38:18 +0100

    Ejercicio 16

--- ExamenRA6/Ejercicio16GestionTienda/src/ejercicio16gestiontienda/Ejercicio16GestionTienda.java
+++ ExamenRA6/Ejercicio16GestionTienda/src/ejercicio16gestiontienda/Ejercicio16GestionTienda.java
@@ -7,6 +7,7 @@ package ejercicio16gestiontienda;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.util.Iterator;
 
 /**
  *
@@ -43,7 +44,7 @@ public class Ejercicio16GestionTienda {
         System.out.println("Elija una de las siguientes opciones:");
         System.out.println("1. Introducir productos en la lista");
         System.out.println("2. Visualizar la lista de productos");
-        System.out.println("3. Eliminar la lista de productos");
+        System.out.println("3. Eliminar productos de la lista");
         System.out.println("4. Volver al menú principal");
         System.out.println("------------------------------------");
     }
@@ -61,7 +62,7 @@ public class Ejercicio16GestionTienda {
 
     /**
      * Método que pide un número al usuario
-     * 
+     *
      * @return el número que introduzca el usuario
      */
     public static int pedirNum() {
@@ -79,10 +80,10 @@ public class Ejercicio16GestionTienda {
 
         return num;
     }
-    
+
     /**
      * Método que pide una cadena al usuario
-     * 
+     *
      * @return la cadena que introduzca el usuario
      */
     public static String pedirCadena() {
@@ -100,10 +101,10 @@ public class Ejercicio16GestionTienda {
 
         return cadena;
     }
-    
+
     /**
      * Método que pide un número decimal al usuario
-     * 
+     *
      * @return el número que introduzca el usuario
      */
     public static float pedirFoat() {
@@ -124,8 +125,8 @@ public class Ejercicio16GestionTienda {
 
     /**
      * Método que contiene el menú principal
-     * 
-     * @param almacen 
+     *
+     * @param almacen
      */
     public static void menuPrincipal(ArrayList<Producto> almacen) {
         int opcion;
@@ -139,6 +140,7 @@ public class Ejercicio16GestionTienda {
                     menuAdmin(almacen);
                     break;
                 case 2:
+                    menuCompra(almacen);
                     break;
                 case 3:
                     System.out.println("Saliendo...");
@@ -148,13 +150,13 @@ public class Ejercicio16GestionTienda {
             }
         } while (opcion != 3);
     }
-    
+
     /**
      * Método que contiene el menú de administración
-     * 
-     * @param almacen 
+     *
+     * @param almacen
      */
-    public static void menuAdmin(ArrayList<Producto> almacen){
+    public static void menuAdmin(ArrayList<Producto> almacen) {
         int opcion;
 
         do {
@@ -166,8 +168,10 @@ public class Ejercicio16GestionTienda {
                     introducirProductos(almacen);
                     break;
                 case 2:
+                    mostrarListaProductos(almacen);
                     break;
                 case 3:
+                    eliminarProductos(almacen);
                     break;
                 case 4:
                     System.out.println("Redirigiendo al menú principal...");
@@ -177,26 +181,157 @@ public class Ejercicio16GestionTienda {
             }
         } while (opcion != 4);
     }
-    
+
     /**
      * Método que introduce productos hasta que el usuario elija parar
-     * 
-     * @param almacen 
+     *
+     * @param almacen
      */
-    public static void introducirProductos(ArrayList<Producto> almacen){
+    public static void introducirProductos(ArrayList<Producto> almacen) {
         String opcion;
         boolean seguir = true;
-        
+
         do {
             System.out.println("Introduzca el nombre, precio y stock en ese orden");
             almacen.add(new Producto(pedirCadena(), pedirFoat(), pedirNum()));
-            
+
             System.out.println("¿Quiere seguir introduciendo productos? s/n");
             opcion = pedirCadena();
             if (opcion.equalsIgnoreCase("n")) {
                 seguir = false;
             }
         } while (seguir);
+    }
+
+    /**
+     * Método que muestra todos los productos de la lista
+     *
+     * @param almacen
+     */
+    public static void mostrarListaProductos(ArrayList<Producto> almacen) {
+        System.out.println("Los productos disponibles son los siguientes:");
+
+        for (int i = 0; i < almacen.size(); i++) {
+            System.out.println((i + 1) + ". " + almacen.get(i).toString());
+        }
+    }
+
+    /**
+     * Método que busca un producto por su nombre, si lo encuentra lo borra, si
+     * no, avisa al usuario
+     *
+     * @param almacen
+     */
+    public static void eliminarProductos(ArrayList<Producto> almacen) {
+        Iterator<Producto> it = almacen.iterator();
+        String busqueda;
+        boolean encontrado = false;
+
+        System.out.println("Introduzca el nombre del producto que quiera eliminar");
+        busqueda = pedirCadena();
+
+        while (!encontrado && it.hasNext()) {
+            Producto aux = it.next();
+
+            if (aux.getNombre().equalsIgnoreCase(busqueda)) {
+                it.remove();
+                encontrado = true;
+
+                System.out.println("Producto con nombre " + busqueda + " eliminado");
+            }
+        }
+        if (!encontrado) {
+            System.out.println("Producto no encontrado");
+        }
+    }
+
+    /**
+     * Método que contiene el menú de compra
+     *
+     * @param almacen
+     */
+    public static void menuCompra(ArrayList<Producto> almacen) {
+        int opcion;
+
+        do {
+
+            mostrarMenuCompra();
+            opcion = pedirNum();
+            switch (opcion) {
+                case 1:
+                    comprarProductos(almacen);
+                    break;
+                case 2:
+                    System.out.println("Redirigiendo al menú principal...");
+                    break;
+                default:
+                    System.out.println("Opción introducida inválida");
+            }
+        } while (opcion != 2);
+    }
+
+    /**
+     * Método que pide al usuario que productos quiere comprar y cuantos, después le muestra
+     * 
+     * @param almacen 
+     */
+    public static void comprarProductos(ArrayList<Producto> almacen) {
+        mostrarListaProductos(almacen);
+        String busqueda;
+        int cantidadCompra;
+        boolean encontrado;
+        String opcion;
+        boolean seguir = true;
+        float totalCompra = 0;
+
+        ArrayList<Producto> cesta = new ArrayList<>();
+
+        Iterator<Producto> it = almacen.iterator();
+
+        do {
+            encontrado = false;
+
+            System.out.println("Introduzca el nombre del producto que quiera comprar");
+            busqueda = pedirCadena();
+            
+            System.out.println("¿Cuantas unidades quiere comprar?");
+            cantidadCompra = pedirNum();
+            
+            while (!encontrado && it.hasNext()) {
+                Producto aux = it.next();
+
+                if (aux.getNombre().equalsIgnoreCase(busqueda) && aux.getStock() >= cantidadCompra) {
+                    aux.setStock(aux.getStock() - cantidadCompra);
+
+                    cesta.add(aux);
+
+                    encontrado = true;
+
+                    System.out.println("Producto con nombre " + busqueda + " añadido a la cesta");
+                } else if (aux.getStock() < cantidadCompra) {
+                    System.out.println("Producto con stock insuficiente");
+                    
+                    System.out.println("Quedan "+ aux.getStock() +" unidades de "+ busqueda +" en el almacen");
+                    
+                    encontrado = true;
+                }
+            }
+            if (!encontrado) {
+                System.out.println("Producto no encontrado");
+            }
+
+            System.out.println("¿Quiere seguir comprando? s/n");
+            opcion = pedirCadena();
+            if (opcion.equalsIgnoreCase("n")) {
+                seguir = false;
+            }
+        } while (seguir);
+
+        for (int i = 0; i < cesta.size(); i++) {
+            totalCompra += cesta.get(i).getPrecio();
+        }
+
+        System.out.println("Su importe total de compra es: " + totalCompra);
     }
 
 }
